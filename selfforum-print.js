52f3cdaac7727e15a825754a808411837d5d6424
Heiko August <post@auge8472.de>
2016-01-26 16:10:38 +0100

    Add: JavaScript zur Unterstützung der Druckfunktion für Postings, Code von @MatthiasApsel

---
+++ selfforum-print.js
@@ -0,0 +1,17 @@
+/**
+ * JS-Anweisungen für den Ausdruck von Postings im SelfHTML-Forum
+ *
+ * @version 0.1
+ * @author M. Apsel
+ * @author H. August <post@auge8472.de>
+ */
+
+
+$(document).ready(function(){
+  $('.forum-links nav ul').append(' <li><a href="" class="print">print</a></li>');
+  
+  $('.forum-links .print').click(function( event ){
+    $(this).parents('.thread-message').addClass('print');
+    event.preventDefault();
+  })
+})
