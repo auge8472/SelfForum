52e32ec89a296327c2de4af5fa2955f6aa2b0dc8
Heiko August <post@auge8472.de>
2016-05-03 18:27:24 +0200

    Change: URLs von Links in Beiträgen werden unterhalb des Beitrags als zusätzliche Fußnoten angezeigt, JS-Code von @MatthiasApsel

--- selfforum-print.js
+++ selfforum-print.js
@@ -13,9 +13,17 @@ $(document).ready(function(){
 		$(this).parents('.thread-message').toggleClass('print');
 		$('body').toggleClass('print-preview');
 		if ($(this).parents('.thread-message').hasClass('print')){
+			var i = 0;
+			$('.print .posting-content').append('<ul class="printfootnoteurls"></ul>');
+			$('.print .posting-content a').each(function(){
+				i++;
+				$(this).after('<sup class="printfootnote">[L'+i+']</sup>');
+				$('.printfootnoteurls').append('<li>[L'+i+'] URL: '+this.href+'</li>');
+			});
 			$(this).text('Druckansicht verlassen');
 			window.print();
 		} else {
+			$('.printfootnoteurls, .printfootnote').remove();
 			$(this).text('Beitrag drucken');
 		}
 		event.preventDefault();
