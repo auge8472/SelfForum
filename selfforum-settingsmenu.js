58508840fd63a28be27b30f62c0adb9da15f218d
Heiko August <post@auge8472.de>
2016-06-02 16:26:10 +0200

    Fix: Semikola als Abschluss einer Anweisung ergänzt; muss zwar nicht, ist dann aber einheitlich

--- selfforum-settingsmenu.js
+++ selfforum-settingsmenu.js
@@ -8,11 +8,11 @@
 
 $(document).ready(function(){
 	var targetlist = '<nav><ul class="tabswitcher"></ul></nav>', targets = '';
-	$('.cf-tab-content').prepend(targetlist)
+	$('.cf-tab-content').prepend(targetlist);
 	$('.cf-tab-pane').each(function(){
 		targets += '<li><a href="#' + this.id + '">' + $(this).children('legend').text() + '</a></li>';
 	})
-	$('.tabswitcher').append(targets)
+	$('.tabswitcher').append(targets);
 	$('.tabswitcher li a').on('click', function(){
 		if ($('#user-data').hasClass('active')) {
 			$('#user-data').removeClass('active');
